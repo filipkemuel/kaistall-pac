0d68fd9130fdf2d2b7d0416971762d50d6598e5f
Filip Kemuel <filip@kemuel.dk>
2022-01-26 10:43:58 +0100

    Updated Repo

--- build_all.sh
+++ build_all.sh
@@ -4,6 +4,7 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 for AUR_PKG in $(cat "${SCRIPTPATH}/BUILDLIST")
 do
-./${SCRIPTPATH}/scripts/build_package.sh ${AUR_PKG}
+cd ${SCRIPTPATH}/scripts
+./build_package.sh ${AUR_PKG}
 [[ $? = 0 ]] || exit 1
 done
